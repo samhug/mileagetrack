606536c9815e26cc9fff84d3cb1d730493da2a77
Samuel Hug <samuel.b.hug@gmail.com>
2014-03-28 12:20:27 -0700

    Add test helpers

---
+++ test/server/helpers.js
@@ -0,0 +1,88 @@
+var mongoose = require('mongoose'),
+    Promise  = require('mpromise'),
+    request  = require('supertest'),
+    app      = require('../../server'),
+    Q        = require('q'),
+    User     = mongoose.model('User'),
+    Vehicle  = mongoose.model('Vehicle');
+
+var TestUser = function(_unique) {
+
+  var unique = _unique || this._makeRandom();
+
+  var userData = {
+    provider: 'local',
+    name: 'TestUser '+unique,
+    email: 'test'+unique+'@test.com',
+    password: 'test'
+  };
+
+  this.vehicles = [];
+  this.promises = [];
+
+  var p = this._addPromise();
+
+  var this_ = this;
+
+  // Insert test user data into database
+  User.create(userData, function (err, userModel) {
+    if (err) throw err;
+
+    this_.userModel = userModel;
+    this_.request = request.agent(app);
+
+    this_.request
+      .post('/api/session')
+      .send({ email: userModel.email, password: userData.password })
+      .end(function(err, res) {
+        if (err) throw err;
+        p.resolve();
+      });
+  });
+}
+
+TestUser.prototype._makeRandom = function(l) {
+  var LENGTH = l || 8;
+  var CHARSPACE = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890';
+
+  var index;
+  var buf = '';
+
+  for (var i=0; i<LENGTH; i++) {
+    index = (Math.random() * (CHARSPACE.length - 1)).toFixed(0);
+    buf += CHARSPACE[index];
+  }
+
+  return buf;
+}
+
+TestUser.prototype._addPromise = function() {
+  var p = new Promise();
+  this.promises.push(p);
+  return p;
+};
+
+TestUser.prototype.createVehicle = function(_unique) {
+  var unique = _unique || this._makeRandom();
+
+  var this_ = this;
+  var p = this._addPromise();
+
+  return Vehicle.create({
+    name: 'TestVehicle '+unique,
+    owner: this_.userModel._id,
+  }, function(err, vehicleModel) {
+    if (err) throw err;
+    this_.vehicles.push(vehicleModel);
+    p.resolve();
+  });
+};
+
+TestUser.prototype.wait = function(fn) {
+  Q.all(this.promises).then(fn);
+};
+
+module.exports = {
+  TestUser: TestUser,
+};
+
--- test/server/vehicle/api.js
+++ test/server/vehicle/api.js
@@ -4,130 +4,44 @@ var should   = require('should'),
     mongoose = require('mongoose'),
     request  = require('supertest'),
     Promise  = require('mpromise'),
-    app      = require('../../../server');
+    app      = require('../../../server'),
+    helpers  = require('../helpers');
 
 var User     = mongoose.model('User'),
     Vehicle  = mongoose.model('Vehicle');
 
-var createTestUsers = function() {
-
-    var authRequest1, authRequest2, user1, user2;
-
-    var testUserData = [
-      {
-        provider: 'local',
-        name: 'Test User',
-        email: 'test@test.com',
-        password: 'test'
-      },
-      {
-        provider: 'local',
-        name: 'Test User 2',
-        email: 'test2@test.com',
-        password: 'test'
-      }
-    ];
-
-    var promise1 = new Promise();
-    var promise2 = new Promise();
-
-    // Insert test user data into database
-    User.create(testUserData, function (err, _user1, _user2) {
-
-      user1 = _user1;
-      user2 = _user2;
-
-
-      authRequest1 = request.agent(app);
-      authRequest1
-        .post('/api/session')
-        .send({ email: user1.email, password: 'test' })
-        .end(function(err, res) {
-          promise1.resolve(err);
-        });
-
-      authRequest2 = request.agent(app);
-      authRequest2
-        .post('/api/session')
-        .send({ email: user2.email, password: 'test' })
-        .end(function(err, res) {
-          promise2.resolve(err);
-        });
-
-    });
-
-
-    return promise1.chain(promise2).then(function () {
-      return [authRequest1, authRequest2, user1, user2];
-    });
-};
 
 describe('Vehicles API', function() {
 
-  var user1Request, user2Request, user1, user2;
-  var vehicle;
+  var testUser1, testUser2;
   
   before(function(done) {
 
     User.remove().exec();
     Vehicle.remove().exec();
 
-    createTestUsers().then(function(args) {
+    testUser1 = new helpers.TestUser();
+    testUser1.wait(function() {
+      testUser1.createVehicle();
+      testUser1.createVehicle();
+    });
 
-      user1Request = args[0];
-      user2Request = args[1];
-      user1 = args[2];
-      user2 = args[3];
+    testUser2 = new helpers.TestUser();
+    testUser2.wait(function() {
+      testUser2.createVehicle();
+    });
 
-
-      return Vehicle.create({
-        name: '2002 Ford Escape',
-        owner: user1._id,
-      }, function(err) {
-        if (err) throw err;
+    testUser1.wait(function() {
+      testUser2.wait(function() {
+        done();
       });
-    })
-    .then(function() {
-      return Vehicle.create({
-        name: 'User 2\'s vehicle',
-        owner: user2._id,
-      }, function(err) {
-        if (err) throw err;
-      });
-    })
-    .then(function() {
-      return Vehicle.create({
-        name: '2014 Volkswagen Jetta',
-        owner: user1._id,
-      }, function(err) {
-        if (err) throw err;
-      });
-    })
-    .then(function(_vehicle) {
-      vehicle = _vehicle;
-      done();
     });
 
   });
 
-  /*
-  describe('POST /api/vehicles', function() {
-    it('should insert vehicle into db', function(done) {
-      userRequest
-        .post('/api/vehicles')
-        .send({ owner: testUser._id, name: 'Mythical Test Vehicle' })
-        .expect(200)
-        .expect('content-type', /json/)
-        .end(function (err, res) {
-          if (err) return done(err);
-          res.body.should.be.instanceof(Array);
-          done();
-        });
-    });
-  });
-  */
 
   describe('GET /api/vehicle', function() {
+
 
     it('should respond with 401 error', function(done) {
       request.agent(app)
@@ -140,7 +54,7 @@ describe('Vehicles API', function() {
     });
 
     it('should respond with JSON array', function(done) {
-      user1Request
+      testUser1.request
         .get('/api/vehicle')
         .expect(200)
         .expect('content-type', /json/)
@@ -153,7 +67,7 @@ describe('Vehicles API', function() {
 
     // Assert that user 2 can only retrieve the 1 vehicle he owns.
     it('should respond with JSON array of length 1', function(done) {
-      user2Request
+      testUser2.request
         .get('/api/vehicle')
         .expect(200)
         .expect('content-type', /json/)
@@ -167,8 +81,8 @@ describe('Vehicles API', function() {
 
   describe('GET /api/vehicle/:id', function() {
     it('should respond with JSON object', function(done) {
-      user1Request
-        .get('/api/vehicle/'+vehicle._id)
+      testUser1.request
+        .get('/api/vehicle/'+testUser1.vehicles[0]._id)
         .expect(200)
         .expect('content-type', /json/)
         .end(function (err, res) {

8156469ea461c0a792225235612903bbaf738868
Samuel Hug <samuel.b.hug@gmail.com>
2014-03-29 12:40:06 -0700

    Use angular-bootstrap data input

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('mileagetrackApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ui.bootstrap'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider

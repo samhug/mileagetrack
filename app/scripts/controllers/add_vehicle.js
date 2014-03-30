99654cccfb0acd7153db0152d995b83d6eb2f437
Daniel Hug <daniel@hugwebdesign.com>
2014-03-30 16:35:56 -0700

    consistent code style
    No need for the `typeof` check; the return value of a function is never "not defined" (`typeof fn() === 'undefined'`) though it may be `undefined` (`fn() === undefined`).

--- app/scripts/controllers/add_vehicle.js
+++ app/scripts/controllers/add_vehicle.js
@@ -1,28 +1,27 @@
-'use strict'
+'use strict';
 
 angular.module('mileagetrackApp')
   .controller('VehicleAddCtrl', function ($scope, $location, Vehicle) {
     $scope.alerts = [];
 
-    if (typeof $location.search('new_user') !== 'undefined') {
-      $scope.alerts.push({ type: "info", msg: "Add a vehicle to get started!"}); 
+    if ($location.search('new_user') !== undefined) {
+      $scope.alerts.push({ type: 'info', msg: 'Add a vehicle to get started!'}); 
     }
 
     // Add Vehicle Scope
-    var vscope;
-    $scope.addVehicle = vscope = {
+    var vscope = $scope.addVehicle = {
       errors: {},
     };
 
     vscope.vehicleAdd = function(form) {
       vscope.submitted = true;
 
-      if(form.$valid) {
+      if (form.$valid) {
         Vehicle.save(vscope.vehicle, function() {
           vscope.message = 'Vehicle Added';
           $location.path("/");
         }, function () {
-          vscope.message = 'Failed! '+vscope.vehicle;
+          vscope.message = 'Failed! ' + vscope.vehicle;
         });
       }
     };

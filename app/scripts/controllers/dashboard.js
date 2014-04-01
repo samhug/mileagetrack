9d610e629c2a901a6f50f0043b3d226e50ba54ae
Samuel Hug <samuel.b.hug@gmail.com>
2014-04-01 14:56:59 -0700

    Add vehicle select menu on dashboard.
    Close #5

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -24,9 +24,9 @@ angular.module('mileagetrackApp')
 
     $scope.vehicles = Vehicle.list();
 
+    // If the user doesn't have any vehicles registered, redirect them to the
+    // add vehicle page with the new_user param.
     $scope.vehicles.$promise.then(function () {
-      // If the user doesn't have any vehicles registered, redirect them to the
-      // add vehicle page with the new_user param.
       if ($scope.vehicles.length === 0) {
         $scope.message = 'test';
         $location.search('newUser');
@@ -35,17 +35,27 @@ angular.module('mileagetrackApp')
       }
     });
 
-    $scope.vehicle = Vehicle.get({id: $scope.currentUser.defaultVehicle});
 
+    // Vehicle Select Dropdown
+    $scope.selectVehicle = function(vehicle) {
+      $scope.vehicle = vehicle;
+      $scope.entries = Entry.query({ vehicle: $scope.vehicle._id });
+    };
+    $scope.addVehicle = function() {
+      $location.path('/vehicle/add');
+    };
+
+
+    $scope.vehicle = Vehicle.get({id: $scope.currentUser.defaultVehicle});
     $scope.vehicle.$promise.then(function () {
       $scope.entries = Entry.query({ vehicle: $scope.vehicle._id });
     });
 
 
+    // New Mileage Entry Form
     $scope.newEntryForm = {
       date: dateFilter(new Date(), 'yyyy-MM-dd'),
     };
-
     $scope.newEntry = function(form) {
       var e = angular.extend({ vehicle: $scope.vehicle._id }, form);
       var entry = new Entry(e);

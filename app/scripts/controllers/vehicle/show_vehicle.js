6c6ec59ccdc080a75a8e659df1a68fc1912d2124
Samuel Hug <samuel.b.hug@gmail.com>
2014-04-07 23:30:38 -0700

    Add average MPG and average $/Mile displays.
    Reference #18

--- app/scripts/controllers/vehicle/show_vehicle.js
+++ app/scripts/controllers/vehicle/show_vehicle.js
@@ -3,16 +3,33 @@
 angular.module('mileagetrackApp')
   .controller('VehicleShowCtrl', function ($scope, Entry) {
 
+    $scope.mpgAvg = 'N/A';
+    $scope.$pmAvg = 'N/A';
+
     var updateEntries = function() {
       $scope.entries = Entry.query({ vehicle: $scope.vehicle._id }, function () {
+
+        var mpgTotal = 0;
+        var $pmTotal = 0;
+
+        // Calculate trip and MPG
         var prevOdometer;
         angular.forEach($scope.entries, function (entry, i) {
           if (i > 0) {
             entry.trip = entry.odometer - prevOdometer;
             entry.mpg = entry.trip / entry.gallons;
+            entry.$pm = entry.price / entry.trip;
+
+            mpgTotal += entry.mpg;
+            $pmTotal += entry.$pm;
           }
           prevOdometer = entry.odometer;
         });
+
+        // Caclulate Averages
+        $scope.mpgAvg = mpgTotal / $scope.entries.length;
+        $scope.$pmAvg = $pmTotal / $scope.entries.length;
+
       });
     };
 
